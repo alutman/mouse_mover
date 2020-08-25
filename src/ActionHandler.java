879066b1ff7e5ba5a836bdfee8db2c056d0295c8
alutman <lutman.al@gmail.com>
2020-08-25 15:50:49 +1000

    Remove switch statement

--- src/ActionHandler.java
+++ src/ActionHandler.java
@@ -16,31 +16,26 @@ public class ActionHandler implements ActionListener, WindowStateListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()) {
-            case "start":
-                MouseMover mc;
-                try {
-                    double timeoutSeconds = Math.abs(Double.parseDouble(appFrame.getTimeoutText()));
-                    int timeout = (int)(timeoutSeconds * 1000d);
+        String actionCommand = e.getActionCommand();
+        if ("start".equals(actionCommand)) {
+            MouseMover mc;
+            try {
+                double timeoutSeconds = Math.abs(Double.parseDouble(appFrame.getTimeoutText()));
+                int timeout = (int) (timeoutSeconds * 1000d);
 
-                    int moveRange = Math.abs(Integer.parseInt(appFrame.getMoveRangeText()));
-                    mc = new MouseMover(timeout, moveRange, appFrame.isAnchorChecked(), appFrame.isClickChecked());
-                } catch (IllegalArgumentException ex) {
-                    appFrame.setStatus("invalid entry", ex.getMessage());
-                    return;
-                }
-                appFrame.startThread(new Thread(mc));
-
-                break;
-            case "stop":
-                appFrame.stopThread();
-                break;
-            case "exit":
-                appFrame.exit();
-                break;
-            case "restore":
-                appFrame.restore();
-                break;
+                int moveRange = Math.abs(Integer.parseInt(appFrame.getMoveRangeText()));
+                mc = new MouseMover(timeout, moveRange, appFrame.isAnchorChecked(), appFrame.isClickChecked());
+            } catch (IllegalArgumentException ex) {
+                appFrame.setStatus("invalid entry", ex.getMessage());
+                return;
+            }
+            appFrame.startThread(new Thread(mc));
+        } else if ("stop".equals(actionCommand)) {
+            appFrame.stopThread();
+        } else if ("exit".equals(actionCommand)) {
+            appFrame.exit();
+        } else if ("restore".equals(actionCommand)) {
+            appFrame.restore();
         }
     }
 
